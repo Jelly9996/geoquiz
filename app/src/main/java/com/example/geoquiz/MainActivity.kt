00bb29f6614df42a59aeaebb2836f1bf3e292a73
OLGA <ku1460155@gmail.com>
2026-09-22 23:17:09 +0700

    question

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -27,6 +27,16 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+data class Question(val text: String, val correctAnswer: Boolean)
+
+val questionList = listOf(
+    Question("Canberra is the capital of Australia.", true),
+    Question("The Pacific Ocean is larger than the Atlantic Ocean.", true),
+    Question("The Suez Canal connects the Red Sea and the Indian Ocean.", false),
+    Question("The source of the Nile River is in Egypt.", false),
+    Question("The Amazon River is the longest river in the Americas.", true),
+    Question("Lake Baikal is the world's oldest and deepest freshwater lake.", true)
+)
 @Composable
 fun GeoQuizScreen(innerPadding: Modifier) {
 
