2b04a23a1894978a5b9fdee69ccb6b6d5e9747ae
OLGA <ku1460155@gmail.com>
2026-09-23 00:11:28 +0700

    top bar

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -12,6 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.geoquiz.ui.theme.GeoquizTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.TopAppBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,11 +44,21 @@ val questionList = listOf(
     Question("The Amazon River is the longest river in the Americas.", true),
     Question("Lake Baikal is the world's oldest and deepest freshwater lake.", true)
 )
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GeoQuizScreen(innerPadding: Modifier) {
+    val purpleColor = Color(0xFF6200EE)
 
+    Column(modifier = innerPadding.fillMaxSize()) {
+        TopAppBar(
+            title = { Text(text = "GeoQuiz") },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = purpleColor,
+                titleContentColor = Color.White
+            )
+        )
+    }
 }
-
 
 @Preview(showBackground = true)
 @Composable
